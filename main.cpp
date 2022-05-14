b674a4451d47af9684db5d2e02ec0f3145a04320
administrator <administrator@26697.local>
2022-05-14 15:07:28 -0700

    trinagle

--- main.cpp
+++ main.cpp
@@ -2,8 +2,35 @@
 #include <SDL.h>
 #include "Object.h"
 #include "TheFirstMover.h"
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+
+
+void display_func(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+    glVertex3f(0.5, 0.0, 0.5);
+    glVertex3f(0.5, 0.0, 0.0);
+    glVertex3f(0.0, 0.5, 0.0);
+    glVertex3f(0.0, 0.0, 0.5);
+    glEnd();
+    glFlush();
+}
+
+void start_graphics(int* argcp, char* argv []) {
+    glutInit(argcp, argv);
+    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitWindowSize(400, 300);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Hello world!");
+    glutDisplayFunc(display_func);
+    glutMainLoop();
+}
 
 int main(int argc, char* argv []) {
+    start_graphics(&argc, argv);
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
         return 1;
@@ -21,5 +48,6 @@ int main(int argc, char* argv []) {
     }
     end_motion();
     SDL_Quit();
+
     return 0;
 }

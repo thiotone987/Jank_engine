6b452d7b8e7b27d8a968a51c31b54d4055888100
thiotone987 <caryjameschun@gmail.com>
2022-05-14 13:34:19 -0700

    cool

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 #include "Object.h"
 #include "TheFirstMover.h"
 
-int main() {
+int main(int argc, char* argv []) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+    std::cout << "Hello, World!" << std::endl;
     Object obj(PhysicsVector(2, 3), PhysicsVector(1, 2));
     std::cout << "obj: " << obj << std::endl;
     start_motion();
@@ -14,5 +20,6 @@ int main() {
         std::cout << "obj2: " << obj2 << std::endl;
     }
     end_motion();
+    SDL_Quit();
     return 0;
 }

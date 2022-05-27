954325180ed2dc0aa49bdae47acd0f8f84444d33
Jacob Friedman <jacobef2@gmail.com>
2022-05-27 04:05:31 -0700

    rename cmake project to 'Jank_engine'

--- Graphics.h
+++ Graphics.h
@@ -2,8 +2,8 @@
 // Created by Jacob Friedman on 5/26/22.
 //
 
-#ifndef SDL2_BASIC_SETUP_GRAPHICS_H
-#define SDL2_BASIC_SETUP_GRAPHICS_H
+#ifndef JANK_ENGINE_GRAPHICS_H
+#define JANK_ENGINE_GRAPHICS_H
 
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
@@ -17,4 +17,4 @@ void start_graphics(int *argcp, char *argv[]);
 void load_regular_polygon(GLdouble num_sides, GLdouble side_len, GLdouble x, GLdouble y);
 void display_func();
 void display_objects();
-#endif //SDL2_BASIC_SETUP_GRAPHICS_H
+#endif //JANK_ENGINE_GRAPHICS_H
